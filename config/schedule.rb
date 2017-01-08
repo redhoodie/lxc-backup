c806cdf264604344e36303b7c39bf76c8cd673fd
Michael Brown <redhoodie@gmail.com>
2017-01-08 18:49:08 +1300

    Update whenever schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,8 +3,15 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+
+set :output, '/var/log/lxc-backup-cron.log'
 every 1.days do
-  command "./backup.rb"
+  command "/root/lxc-backup/backup.rb -n app1"
+  command "/root/lxc-backup/backup.rb -n app2"
+  command "/root/lxc-backup/backup.rb -n app3"
+  command "/root/lxc-backup/backup.rb -n database"
+  command "/root/lxc-backup/backup.rb -n loadbalancer"
+  command "/root/lxc-backup/backup.rb -n web1"
 end
 
 # Learn more: http://github.com/javan/whenever
