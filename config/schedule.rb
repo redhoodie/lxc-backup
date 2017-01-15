2f38ddf29d12bddb2200d563a053721b8e1081de
Michael Brown <redhoodie@gmail.com>
2017-01-16 11:42:54 +1300

    Space out backup jobs in schedule to try and fix them dying.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,13 +5,32 @@
 
 
 set :output, '/var/log/lxc-backup-cron.log'
-every 1.days do
+
+every 1.days, :at => '1:00 am' do
   command "/root/lxc-backup/backup.rb -n app1 --silent"
+end
+
+every 1.days, :at => '1:30 am' do
   command "/root/lxc-backup/backup.rb -n app2 --silent"
+end
+
+every 1.days, :at => '2:00 am' do
   command "/root/lxc-backup/backup.rb -n app3 --silent"
+end
+
+every 1.days, :at => '2:30 am' do
   command "/root/lxc-backup/backup.rb -n app4 --silent"
+end
+
+every 1.days, :at => '3:00 am' do
   command "/root/lxc-backup/backup.rb -n database --silent"
+end
+
+every 1.days, :at => '3:30 am' do
   command "/root/lxc-backup/backup.rb -n loadbalancer --silent"
+end
+
+every 1.days, :at => '4:00 am' do
   command "/root/lxc-backup/backup.rb -n web1 --silent"
 end
 
