070e4994100994320caa457a3ef9c14f309518f0
Michael Brown <redhoodie@gmail.com>
2021-01-18 04:58:21 +0000

    Add -r --remote option

--- copy-status.rb
+++ copy-status.rb
@@ -8,16 +8,17 @@ DEFAULT_SAMPLE_SIZE = 3
 LINES_PER_CONTAINER = 2
 LXC_CONTAINER_COPYING_TAG = 'copying'
 
-
-COMMAND = %{lxc monitor | grep --line-buffered fs_progress | head -%d | sed --regexp-extended "s/.*?'(.*?)'/\\1/g"}
-
 @sample_size = DEFAULT_SAMPLE_SIZE
-@config = {:timeout => nil}
+@config = {:timeout => nil, :remote => nil}
 
 cli = OptionParser.new do |options|
   options.banner = "Usage: copy-satus.rb [options]"
-  options.define '-t', '--timeout=SECONDS', 'Timeout after SECONDS'
+
   options.define '-p', '--poll', 'Poll until empty result, implies -t'
+
+  options.define '-r', '--remote=REMOTE', 'Specify a LXD remote, defaults to \'production\''
+
+  options.define '-t', '--timeout=SECONDS', 'Timeout after SECONDS'
 
   options.on('-h', '--help', 'Displays Help') do
     puts options
@@ -27,8 +28,11 @@ end
 
 cli.parse!(into: @config)
 
+LXC_REMOTE = config[:remote]
+COMMAND = %{lxc monitor #{LXC_REMOTE}: | grep --line-buffered fs_progress | head -%d | sed --regexp-extended "s/.*?'(.*?)'/\\1/g"}
+
 def poll_lxc_list
-  containers = `lxc list | grep "#{LXC_CONTAINER_COPYING_TAG}"`.split("\n").collect{|container| container.gsub /^\| (.*?)-copying.*?$/, '\1'}
+  containers = `lxc list #{LXC_REMOTE}: | grep "#{LXC_CONTAINER_COPYING_TAG}"`.split("\n").collect{|container| container.gsub /^\| (.*?)-copying.*?$/, '\1'}
   @containers = (@containers || []) + containers 
   container_count = @containers.count
   container_count = 1 if container_count == 0
